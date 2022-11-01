24e6fd7b193281e81f8c2fb4ebcb215e2837f8ea
Vu Truong Anh <anhvuttruong@gmail.com>
2022-11-01 15:08:27 +0700

    Update face_detect.py

--- Face_Recognition/face_detect.py
+++ Face_Recognition/face_detect.py
@@ -5,11 +5,6 @@ import numpy as np
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-print(device)
-
-prev_frame_time = 0
-new_frame_time = 0
-
 mtcnn = MTCNN(thresholds=[0.7, 0.7, 0.8], keep_all=True, device=device)
 
 cap = cv2.VideoCapture(0)
@@ -25,7 +20,7 @@ while cap.isOpened():
         if boxes is not None:
             for box in boxes:
                 bbox = list(map(int, box.tolist()))
-                frame = cv2.rectangle(frame, (bbox[0],bbox[1]), (bbox[2],bbox[3]), (0, 0, 255), 6)
+                frame = cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 6)
             
     cv2.imshow('Face Detection', frame)
 
